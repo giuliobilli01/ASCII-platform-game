ecaf838fa2dc47a3dfbee3194e80de8fc18095c5
Giulio Billi <72358159+giuliobilli01@users.noreply.github.com>
2022-02-22 12:08:20 +0100

    Added constant header and cpp files

---
+++ src/Constant.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// window dimensions
+
+#define larghezza_max 70
+#define W_CONSOLE 120
+#define H_CONSOLE 20
+
+/**
+ * @brief
+ *
+ */
+enum Direction { RIGHT, LEFT };
+
+enum changeLevelConditions{PREV_LEVEL, NEXT_LEVEL, CURRENT_LEVEL};
